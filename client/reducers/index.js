94358131c95d038ef125f85728d0673b8531fbe6
Dylan Li <dylanli@Dylans-MacBook-Pro.local>
2021-05-22 10:35:41 -0700

    added some reducer descriptions, multischema reducer file, & webpackconfig

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -5,12 +5,15 @@ import schemaReducers from './schemaReducers.js';
 import generalReducers from './generalReducers.js';
 // import queryReducers from './queryReducers.js';
 
+//  import parentSchemasReducers from './databaseReducers.js'
+
 
 // combine reducers
 const combinedReducers = combineReducers({
   general: generalReducers,
   schema: schemaReducers,
   // query: queryReducers,
+  //  parentSchemas: parentSchemasReducers
 });
 
 // make the combined reducers available for import
