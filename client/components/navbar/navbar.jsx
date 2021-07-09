4a81bf87d9062305e708ae76678959b951ce2822
Dylan Li <dylanli@Dylans-MacBook-Pro.local>
2021-07-08 18:24:32 -0700

    stablized tree-view, moved back to material ui

--- client/components/navbar/navbar.jsx
+++ client/components/navbar/navbar.jsx
@@ -3,24 +3,72 @@ import { connect } from 'react-redux';
 import FlatButton from 'material-ui/FlatButton';
 import * as actions from '../../actions/actions';
 
+// Reactjs-Popup Component
+import Popup from 'reactjs-popup';
+
 // styling
 import './navbar.css';
 
 // components
 import Team from './team/team-button.jsx';
 import ExportCode from './export-code/export-button.jsx';
+import TreeView from './tree-view/treeView.jsx';
+import { Dialog } from 'material-ui';
 // import Info from './info/info';
+
+const mapStateToProps = store => ({
+  modalState: store.general.open,
+  schemaObject: store.schema
+});
 
 const mapDispatchToProps = dispatch => ({
   handleNewProject: reset => dispatch(actions.handleNewProject(reset)),
+  saveDatabaseDataInput: schemaObject => dispatch(actions.saveDatabaseDataInput(schemaObject)),
+  handleOpen: () => dispatch(actions.showModal()),
+  handleClose: () => dispatch(actions.hideModal())
 });
 
-const MainNav = ({ handleNewProject }) => (
+
+const MainNav = ({ handleNewProject, modalState, handleClose, handleOpen, saveDatabaseDataInput, schemaObject }) => (
   <div>
     <nav id="navbar">
       <div id="nav-left">
         <img alt="" id="logo" src="./images/Logo.svg" />
         <FlatButton label="New Project" onClick={() => handleNewProject(true)} />
+        <FlatButton 
+          label="Tree View" 
+          onClick={() => {
+            handleOpen();
+            if (schemaObject.name){
+              saveDatabaseDataInput(schemaObject)
+            }}} />
+        <Dialog
+          paperClassName="tree-box"
+          actionsContainerClassName="tree-box2"
+          title='Tree View'
+          modal={false}
+          open={modalState}
+          onRequestClose={handleClose}
+          autoDetectWindowHeight={true} 
+        >
+          <TreeView/>
+        </Dialog>
+        {/* <Popup
+          trigger={<FlatButton className="button" label="Tree View" />}
+          modal>
+          {close => (
+            <div className="modal">
+              <button className="close" onClick={close}>
+                &times;
+              </button>
+              <div className="header"> Tree View </div>
+              <div className="content">
+                {' '}
+                <TreeView/>
+              </div>
+            </div>
+          )}
+        </Popup> */}
         <ExportCode />
       </div>
       <div id="nav-right">
@@ -36,6 +84,6 @@ const MainNav = ({ handleNewProject }) => (
 
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(MainNav);
